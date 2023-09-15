294829505e3427c41bbfcc2290b238bc016ec06e
Whittun <whittun@gmail.com>
2023-09-15 14:57:50 +0300

    feat: add style structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Period from "./components/Period/Period";
 import Topics from "./components/Topics/Topics";
 import Slider from "./components/Slider/Slider";
 
+import "./styles/index.scss";
+
 function App() {
   return (
     <div className="App">
