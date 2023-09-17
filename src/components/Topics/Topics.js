5cee8c781824a3ea9646e6670b0cc4c8e630aeaf
Whittun <whittun@gmail.com>
2023-09-17 17:05:07 +0300

    feat: add styles to the topics section

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -3,37 +3,37 @@ function Topics() {
     <section className="topics">
       <ul className="topics__list">
         <li className="topics__item">
-          <button className="topics__button">1</button>
+          <button className="topics__button"><span className="topics__number">1</span></button>
           <h2 className="topics__title">
             Технологии
           </h2>
         </li>
         <li className="topics__item">
-          <button className="topics__button">2</button>
+          <button className="topics__button"><span className="topics__number">2</span></button>
           <h2 className="topics__title">
             Кино
           </h2>
         </li>
-        <li className="topics__item">
-          <button className="topics__button">3</button>
+        <li className="topics__item topics__item--active">
+          <button className="topics__button"><span className="topics__number">3</span></button>
           <h2 className="topics__title">
             Литература
           </h2>
         </li>
         <li className="topics__item">
-          <button className="topics__button">4</button>
+          <button className="topics__button"><span className="topics__number">4</span></button>
           <h2 className="topics__title">
             Игры
           </h2>
         </li>
         <li className="topics__item">
-          <button className="topics__button">5</button>
+          <button className="topics__button"><span className="topics__number">5</span></button>
           <h2 className="topics__title">
             Спорт
           </h2>
         </li>
         <li className="topics__item">
-          <button className="topics__button">6</button>
+          <button className="topics__button"><span className="topics__number">6</span></button>
           <h2 className="topics__title">
             Наука
           </h2>
